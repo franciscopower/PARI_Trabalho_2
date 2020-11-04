4015ce08e4b368930eb6aa494782c405bf30f901
Francisco Power <franciscopower66@gmai.com>
2020-11-04 15:38:07 +0000

    color segmentation with video

--- color_segmenter.py
+++ color_segmenter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import cv2
+import cv2 as cv
 from functools import partial
 import argparse
 from pprint import pprint
+import numpy as np
 
 # Global variables
 limits_dict = {
@@ -14,7 +15,7 @@ limits_dict = {
     }
 }
 
-def onTrackbar(threshold, I, window_name, channel):
+def onTrackbar(threshold, channel):
     global limits_dict
     
     if channel == 'minBH':
@@ -28,23 +29,7 @@ def onTrackbar(threshold, I, window_name, channel):
     elif  channel == 'minRV':
         limits_dict["limits"]['RV']['min'] = threshold
     elif  channel == 'maxRV':
-        limits_dict["limits"]['RV']['max'] = threshold
-    
-    I_bin = cv2.inRange(
-        I,
-        (
-            limits_dict["limits"]['BH']['min'],
-            limits_dict["limits"]['GS']['min'],
-            limits_dict["limits"]['RV']['min'],
-        ),
-        (
-            limits_dict["limits"]['BH']['max'],
-            limits_dict["limits"]['GS']['max'],
-            limits_dict["limits"]['RV']['max'],
-        )
-    )    
-
-    cv2.imshow(window_name, I_bin)
+        limits_dict["limits"]['RV']['max'] = threshold   
     
     
 def main():
@@ -55,44 +40,72 @@ def main():
     parser.add_argument('-hsv', '--segmentate_in_hsv', help = 'If selected, segmentation will be in hsv', action='store_true')
     args = parser.parse_args()
     
-    hsv_mode = args.segmentate_in_hsv    
-    
-    #Load Image
-    image_filename = "../img/atlas2000_e_atlasmv.png"
-    I = cv2.imread(image_filename, cv2.IMREAD_COLOR)
-    
-    track_names = ['min B', 'max B', 'min G', 'max G', 'min R', 'max R']
-    
-    if hsv_mode:
-        I = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
-        track_names = ['min H', 'max H', 'min S', 'max S', 'min V', 'max V']
-    
-    
-    #Create window
-    cv2.namedWindow(window_name)
+    hsv_mode = args.segmentate_in_hsv 
         
-    #partial function
-    trackbarMinBH = partial(onTrackbar, I=I, window_name=window_name, channel='minBH')
-    trackbarMaxBH = partial(onTrackbar, I=I, window_name=window_name, channel='maxBH')
-    trackbarMinGS = partial(onTrackbar, I=I, window_name=window_name, channel='minGS')
-    trackbarMaxGS = partial(onTrackbar, I=I, window_name=window_name, channel='maxGS')
-    trackbarMinRV = partial(onTrackbar, I=I, window_name=window_name, channel='minRV')
-    trackbarMaxRV = partial(onTrackbar, I=I, window_name=window_name, channel='maxRV')
+    if hsv_mode:
+        track_names = ['min H', 'max H', 'min S', 'max S', 'min V', 'max V']
+    else:
+        track_names = ['min B', 'max B', 'min G', 'max G', 'min R', 'max R']
     
-    cv2.createTrackbar(track_names[0], window_name, 0, 255, trackbarMinBH)
-    cv2.createTrackbar(track_names[1], window_name, 255, 255, trackbarMaxBH)
-    cv2.createTrackbar(track_names[2], window_name, 0, 255, trackbarMinGS)
-    cv2.createTrackbar(track_names[3], window_name, 255, 255, trackbarMaxGS)
-    cv2.createTrackbar(track_names[4], window_name, 0, 255, trackbarMinRV)
-    cv2.createTrackbar(track_names[5], window_name, 255, 255, trackbarMaxRV)
     
-    trackbarMinBH(0)
+    cap = cv.VideoCapture(2)
+    
+    cv.namedWindow(window_name)
+        #partial function
+    trackbarMinBH = partial(onTrackbar, channel='minBH')
+    trackbarMaxBH = partial(onTrackbar, channel='maxBH')
+    trackbarMinGS = partial(onTrackbar, channel='minGS')
+    trackbarMaxGS = partial(onTrackbar, channel='maxGS')
+    trackbarMinRV = partial(onTrackbar, channel='minRV')
+    trackbarMaxRV = partial(onTrackbar, channel='maxRV')
 
-    cv2.waitKey(0)
+    cv.createTrackbar(track_names[0], window_name, 0, 255, trackbarMinBH)
+    cv.createTrackbar(track_names[1], window_name, 255, 255, trackbarMaxBH)
+    cv.createTrackbar(track_names[2], window_name, 0, 255, trackbarMinGS)
+    cv.createTrackbar(track_names[3], window_name, 255, 255, trackbarMaxGS)
+    cv.createTrackbar(track_names[4], window_name, 0, 255, trackbarMinRV)
+    cv.createTrackbar(track_names[5], window_name, 255, 255, trackbarMaxRV)    
+    
+    k = ''
+    while cap.isOpened():
+    
+        _, I_original = cap.read()
+        
+        if hsv_mode:
+            I = cv.cvtColor(I_original, cv.COLOR_BGR2HSV)
+        else:
+            I = np.copy(I_original)
+        
+        I_bin = cv.inRange(
+            I,
+            (
+                limits_dict["limits"]['BH']['min'],
+                limits_dict["limits"]['GS']['min'],
+                limits_dict["limits"]['RV']['min'],
+            ),
+            (
+                limits_dict["limits"]['BH']['max'],
+                limits_dict["limits"]['GS']['max'],
+                limits_dict["limits"]['RV']['max'],
+            )
+        )    
+        
+        I_segmentated = np.copy(I_original)
+        I_segmentated[I_bin == 0] = 0
+
+        cv.imshow(window_name, I_segmentated)    
+
+        k = cv.waitKey(1)
+        if k == ord('q'):
+            break
+        elif k == ord('w') or k==ord('s'):
+            #guardar
+            pass
+            
     
     pprint(limits_dict)
-    
-    
+    cap.release()
+    cv.destroyAllWindows()
     
 
 if __name__ == '__main__':
