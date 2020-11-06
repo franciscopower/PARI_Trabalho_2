6e7ac52eda9a6cf679a60a5bcfcc6a57cf07be59
Francisco Power <franciscopower66@gmai.com>
2020-11-06 19:26:18 +0000

    degubbing, error in AR mode

--- ar_paint.py
+++ ar_paint.py
@@ -51,15 +51,17 @@ def findCentroid(I, limits_dict):
 
     return (x,y)
 
-def keyboardMapping(k, I, frame, AR, brush_size, opacity):
+def keyboardMapping(k, I, frame, AR, brush_size, opacity, clr):
+    color = clr
+    
     if k == ord('r'):
         color = (0, 0, 255)
     elif k == ord('g'):
         color = (0, 255, 0)
     elif k == ord('b'):
         color = (255, 0, 0)
-    # elif k == ord('e'):
-    #     color = (255,255,255) #extra, dá bastante mais trabalho
+    elif k == ord('e'):
+        color = (255,255,255)
     elif k == ord('c'):
         if AR:
             I = np.ones(frame.shape)*0
@@ -71,29 +73,29 @@ def keyboardMapping(k, I, frame, AR, brush_size, opacity):
     elif k == 45:
         if brush_size > 1:
             brush_size += -1 
-    elif k == ord('w') or k == ord('s'):
+    elif k == ord('w') or k == ord('s'): 
         ct = (str(ctime())).replace(' ', '_')
         filename = "drawing_{}.png".format(ct)
         save_state = cv.imwrite(filename, I)
         #add delay ?
         if save_state:
-            print("Image capture saved") # aplicar colorama stuff
+            print("Image capture saved") # apply colorama stuff
         else:
             print("Error: Image capture not saved")
-    else:
+    elif k == ord('p') or k == ord('k'):
         color = (0, 0, 0)
         
         #add high and low  opacity
             
     return color, brush_size, I
 
-def paint(frame, p1, p2, color, brush_size, AR, opacity):
-    I = np.ones(frame.shape)*255
-    
+def paint(frame, I, p1, p2, color, brush_size, AR, opacity):    
     cv.line(I, p1, p2, color, brush_size)
     
     if AR:
-        I = cv.addWeighted(frame, 1, I, opacity, 0)
+        I[I==255] = 0
+        print(type(I))
+        # I = cv.addWeighted(frame, 1, I, opacity, 0)
     
     return I
 
@@ -129,9 +131,10 @@ def main():
 
     # Window names
     paint_window = "Paint"
-    live_window = "Live feed"
+    live_window = "Live feed" 
         
-    #inicialização do pincel    
+        
+    #brush initializarion    
     color = (0,0,0)
     brush_size = 2
     opacity = 0.8
@@ -139,10 +142,11 @@ def main():
     camera_number = int(args.camera_number)
     cap = cv.VideoCapture(camera_number)
     _,frame = cap.read()
-        
-    # ------ começa o video -------------------
+    I = np.ones(frame.shape)*255    
+    # ------ video starts-------------------
     k=''
     p1 = (0,0)
+    
     
     while cap.isOpened() and k != ord('q'):
     
@@ -152,11 +156,9 @@ def main():
         p2 = findCentroid(frame, limits_dict)
         
         if p1 != (0,0):
-            I = paint(frame, p1, p2, color, brush_size, AR, opacity)
+            I = paint(frame, I, p1, p2, color, brush_size, AR, opacity)
         
- 
-        
-        color, brush_size, I = keyboardMapping(k, I, frame, AR, brush_size, opacity)
+        color, brush_size, I = keyboardMapping(k, I, frame, AR, brush_size, opacity, color)
         
         cv.imshow(live_window, frame)
         cv.imshow(paint_window, I)
@@ -164,6 +166,7 @@ def main():
         p1 = p2
         
         k = cv.waitKey(1)
+        
     
     
         
